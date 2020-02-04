4a84be5036314dcf3301c6acbf947a86e9f58f26
perqin <perqinxie@gmail.com>
2020-02-04 17:39:07 +0800

    feat: Update perqin/go-shadowsocks2

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.13
 require (
 	fyne.io/fyne v1.2.2
 	github.com/getlantern/systray v0.0.0-20200109124156-9abdfb6448b3
-	github.com/perqin/go-shadowsocks2 v0.0.12-0.20200118081721-843e4c574696
-	github.com/shadowsocks/go-shadowsocks2 v0.0.11 // indirect
+	github.com/perqin/go-shadowsocks2 v0.0.12-0.20200204091020-88e096e6d650
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
 )
--- shadowsocks.go
+++ shadowsocks.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"github.com/perqin/go-shadowsocks2"
 )
@@ -14,9 +13,8 @@ func runShadowsocks(flags shadowsocks2.Flags) (err error) {
 		err = errors.New("an instance is already running")
 		return
 	}
-	// cancel will be nil if err is not nil
-	ctx, cancelFunc := context.WithCancel(context.Background())
-	if err = shadowsocks2.Run(flags, ctx); err != nil {
+	cancelFunc, err := shadowsocks2.Run(flags)
+	if err != nil {
 		return
 	}
 	cancel = func() error {
